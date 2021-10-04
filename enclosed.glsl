061bc75ce47576d8e8010690a1a6eade625f383c
Spruce Campbell <spruce@lvl12.uk>
2021-10-04 20:07:29 +0100

    Final changes

---
+++ enclosed.glsl
@@ -0,0 +1,72 @@
+#version 300 es
+
+precision mediump float;
+out vec4 outColor;
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+
+vec3 lerp(vec3 colorone, vec3 colortwo, float value)
+{
+    return (colorone + value*(colortwo-colorone));
+}
+
+void main() {
+	vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+	uv = uv * 2.0 - 1.0;
+    uv.x *= u_resolution.x / u_resolution.y;
+
+	uv = (uv * pow(2.7418, 2.));
+
+    // Time varying pixel color
+    vec3 col = vec3(abs(uv.x), abs(uv.y), 0.);
+    
+    float x = 0.;
+    float y = 0.;
+    int iMax = 240;
+    int i = 0;
+    while(x*x + y*y <= 4. && i < iMax)
+	{
+		float tempX = x*x - y*y + uv.x;
+        y = 2.*x*y + uv.y;
+        x = tempX;
+        i+=1;
+	}
+    
+    vec3 color = vec3(1.);
+    float dist = sqrt((uv.x)*(uv.x) + uv.y*uv.y);
+    bool onLine1 = false;
+    bool onLine2 = false;
+    bool onLine3 = false;
+    
+    if(abs(uv.x)<0.01){
+        onLine1 = true;
+    }
+    if(abs(uv.y)<0.01){
+        onLine1 = true;
+    }
+    if(abs(mod(uv.x, 1.))<0.04  && abs(uv.y) <0.07){
+        onLine1 = true;
+    }
+    if(abs(mod(uv.y, 1.))<0.04  && abs(uv.x) <0.07){
+        onLine1 = true;
+    }
+
+    if(abs(length(uv) - 2.)<0.01){
+        onLine2 = true;
+    }
+
+    if(onLine1){
+        color = vec3(0.);
+    } else if(onLine2){
+        color = vec3(0.);
+    }
+
+    float image = (float(i)/ 120.5) - (30./120.5);
+    color *= (vec3(1.) - image);
+
+    // Output to screen
+    outColor = vec4(color,1.0);
+}
